597e10b80d68e623c2ff10170965e123d11364a7
Yuan Yao <melofoyyyy@outlook.com>
2020-12-13 23:30:22 -0800

    ...

--- routine-plus/src/components/create-block.component.js
+++ routine-plus/src/components/create-block.component.js
@@ -20,9 +20,9 @@ export default class CreateBlock extends Component {
     this.state = {
       image: null,
       task: '',
-      routine: '',
+      routine: 'daily',
       date: new Date(),
-      status: 'Todo',
+      status: 'Backlog',
     }
   }
   //event listener to change name of span
@@ -37,11 +37,8 @@ export default class CreateBlock extends Component {
       fileChosen.textContent = file[0].name;
     }
   }
-
   onChangeImage() {
-
     const file = document.getElementById("inputGroupFile01").files;
-
     const formData = new FormData();
     if (file.length !== 0) {
       formData.append("img", file[0]);
@@ -56,7 +53,8 @@ export default class CreateBlock extends Component {
         image: file[0].name
       })
     }
-  }
+    alert("Image Uploaded!");
+  } 
   onChangeTask(e) {
     this.setState({
       task: e.target.value
@@ -66,9 +64,6 @@ export default class CreateBlock extends Component {
     this.setState({
       routine: e.target.value
     })
-    //after entering routine, image function will be called
-    //gets rid of the upload button
-    this.onChangeImage();
   }
   onChangeDate(date) {
     this.setState({
@@ -85,7 +80,7 @@ export default class CreateBlock extends Component {
       status: this.state.status,
     }
     if (block.image === null) {
-      alert("Must have image!")
+      alert("Upload The Image!")
       return
     }
     axios.post('http://localhost:5000/blocks/add', block, { headers: { "x-auth-token": localStorage.getItem("auth-token") } }) // by junfeng
@@ -96,64 +91,56 @@ export default class CreateBlock extends Component {
     window.location = '/blocks';
   }
   render() {
-
+    const routines = ["daily", "weekly", "monthly"]
     return (
       <div className="create-container d-flex justify-content-center align-items-center" >
-      <div className="test">
-        <form className="create-form" onSubmit={this.onSubmit}>
-        <div className="form-group">
-          <h2> New Routine </h2>
-
-
-            <div className="mb-3">
-              <Form.File id="formcheck-api-regular">
-                <Form.File.Label className="psuedo-upload btn btn-neon" htmlFor="inputGroupFile01">Select an Image</Form.File.Label> {/* custom upload button, actually a label*/}
-                <span id="file-chosen" style={{paddingLeft: "10px", color: "grey"}}> No file chosen</span> {/* file-chosen*/}
-                <Form.File.Input id="inputGroupFile01" className="input-field" onChange={this.onChangeFileInputName} hidden/> {/* actual-btn*/}
-              </Form.File>
+        <div className="test">
+          <form className="create-form" onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <h2> New Routine </h2>
+                <div className="mb-3">
+                  <Form.File id="formcheck-api-regular">
+                    <Form.File.Label className="psuedo-upload btn btn-neon" htmlFor="inputGroupFile01">Select an Image</Form.File.Label> {/* custom upload button, actually a label*/}
+                    <span id="file-chosen" style={{paddingLeft: "10px", color: "grey"}}> No file chosen</span> {/* file-chosen*/}
+                    <Form.File.Input id="inputGroupFile01" className="input-field" onChange={this.onChangeFileInputName} hidden/> {/* actual-btn*/}
+                  </Form.File>
+                </div>
+                <button type="button" className="btn btn-neon" onClick={this.onChangeImage}>Upload</button>
+              {/* gets rid of upload button*/}
+              <img id="img" style={{ display: "none" }} alt=""></img>
             </div>
-
-          {/* gets rid of upload button*/}
-          <img id="img" style={{ display: "none" }} alt=""></img>
-
+            <div className="form-group">
+              <input type="text"
+                required
+                className="input-field"
+                placeholder="Task"
+                value={this.state.task}
+                onChange={this.onChangeTask}
+              />
+            </div>
+            <div className="form-group"> 
+              <label>Routine: </label>
+              <select required className="form-control" value={this.state.routine} onChange={this.onChangeRoutine}> {
+                routines.map(function(routine) {
+                  return <option key={routine} value={routine}>{routine}</option>;
+                })}
+              </select>
+            </div>
+            <div className="form-group">
+              <label className="date-label">Date: </label>
+              <div>
+                <DatePicker className="input-date-create"
+                  selected={this.state.date}
+                  onChange={this.onChangeDate}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <input type="submit" value="Create Routine" className="btn btn-neon" />
+            </div>
+            <button class="btn btn-cancel" onClick={this.onButtonCancel}>Cancel</button>
+          </form>
         </div>
-        <div className="form-group">
-
-          <input type="text"
-            required
-            className="input-field"
-            placeholder="Task"
-            value={this.state.task}
-            onChange={this.onChangeTask}
-          />
-        </div>
-        <div className="form-group">
-
-          <input type="text"
-            required
-            className="input-field"
-            placeholder="Routine"
-            value={this.state.routine}
-            onChange={this.onChangeRoutine}
-          />
-        </div>
-        <div className="form-group">
-          <label className="date-label">Date: </label>
-          <div>
-            <DatePicker className="input-date-create"
-              selected={this.state.date}
-              onChange={this.onChangeDate}
-            />
-          </div>
-        </div>
-        <div className="form-group">
-          <input type="submit" value="Create Routine" className="btn btn-neon" />
-        </div>
-        <button class="btn btn-cancel" onClick={this.onButtonCancel}>Cancel</button>
-      </form>
-      </div>
-
-
       </div>
     )
   }
